864486acd4a1dabfc4850dd5b547b964e3fa0c30
Sintia Saado <sintia@macbookair.home>
2026-01-08 15:21:01 +0100

    Changed footer in index

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 //import "./style.css";
-import { checkout } from "./checkout";
+import { checkout } from "./utils/checkout";
 import { initHamburgerNav } from "./hamburgerNav";
 import "./scss/main.scss";
 
