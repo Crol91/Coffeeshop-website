e37670e69e70d9d5cbd1ab7b0e397b9b198e30b3
marcuswesterblom <marcus.vesterblom@gmail.com>
2026-01-13 10:28:21 +0100

    Updated cartLogic

--- src/logic/cartLogic.ts
+++ src/logic/cartLogic.ts
@@ -8,13 +8,22 @@ cartItems.forEach(item => {
     const minus = itemEl.querySelector(".cartMinus") as HTMLButtonElement;
     const plus = itemEl.querySelector(".cartPlus") as HTMLButtonElement;
     const quantity = itemEl.querySelector(".cartQuantity") as HTMLSpanElement;
+    if (!minus || !plus || !quantity) return;
 
         minus.addEventListener("click", () => {
-            if (item.quantity > 1) {
                 item.quantity--;
+                if(item.quantity <= 0){
+                    itemEl.remove();
+
+                    const index = cartItems.findIndex(i => i.id === item.id);
+                    if(index !== -1) {
+                        cartItems.splice(index, 1);
+                    }
+                    updateTotal(cartItems);
+                    return;
+                }
                 quantity.textContent = item.quantity.toString();
                 updateTotal(cartItems);
-            }
         });
         plus.addEventListener("click", () => {
                 item.quantity++;
