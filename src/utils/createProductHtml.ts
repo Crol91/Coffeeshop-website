6932a6d6e816de357c83c94db6ddefcebef8fc95
Crol91 <karl.e.andersson@hotmail.com>
2026-01-13 15:45:17 +0100

    Added function to create HTML for products

---
+++ src/utils/createProductHtml.ts
@@ -0,0 +1,37 @@
+import { Coffee } from "../models/Coffee";
+import { coffeeList } from "../data/coffees";
+
+const productContainer = document.getElementById("productContainer");
+
+if (productContainer) {
+  productContainer.innerHTML = "";
+
+  coffeeList.forEach((coffee) => {
+    const productCard = createProductCard(coffee);
+    productContainer.appendChild(productCard);
+  });
+}
+
+function createProductCard(coffee: Coffee): HTMLElement {
+  const card = document.createElement("div");
+  card.className = "product-card";
+  card.innerHTML = `<img src="${coffee.image}" alt="${
+    coffee.name
+  }" class="product-image"/>
+    <div class="product-info">
+        <h3>${coffee.name}</h3>
+        <p><strong>${coffee.price} :-</strong></p>
+        <div class="stock-status ${
+          coffee.inStock ? "in-stock" : "out-of-stock"
+        }">
+        <span class="inStockCircle"></span>
+        <span>${coffee.inStock ? "Finns i lager" : "Finns ej i lager"}</span>
+            </div>
+            <button class="add-to-cart">Lägg i varukorgen
+            <span class="addToCartIcon">
+  <img src="../assets/icons/icon-cart.svg" alt="a cart icon" />
+</span>
+            </button>
+    </div>`;
+  return card;
+}
