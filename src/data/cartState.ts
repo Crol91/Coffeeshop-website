4def28249187be3640f96eefca09886b756b6a18
marcuswesterblom <marcus.vesterblom@gmail.com>
2026-01-13 14:18:28 +0100

    Change product images

--- src/data/cartState.ts
+++ src/data/cartState.ts
@@ -1,11 +1,11 @@
 import type { CartItem } from "../types/CartItem";
-import dark_roast from "../assets/products/dark_roast.jpeg";
-import vanilla  from "../assets/products/vanilla.jpeg";
+import darkRoast from "../assets/coffeeImages/darkRoast.jpeg";
+import vanilla  from "../assets/coffeeImages/vanilla.jpeg";
 
 export let cartItems: CartItem[] = [
     {
         id: "coffee-1",
-        thumbnail: dark_roast,
+        thumbnail: darkRoast,
         name: " Dark Roast",
         price: 199,
         quantity: 1
