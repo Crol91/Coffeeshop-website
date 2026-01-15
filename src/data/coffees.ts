2674b90c80c9b0e2bba44b49669c3647f9337a61
Crol91 <karl.e.andersson@hotmail.com>
2026-01-15 15:00:30 +0100

    Added more styling to product cards

--- src/data/coffees.ts
+++ src/data/coffees.ts
@@ -2,19 +2,24 @@ import { Coffee } from "../models/Coffee";
 
 export const coffeeList: Coffee[] = [
   new Coffee(
-    "../assets/coffeeImages/caramelDark.jpeg",
+    "/src/assets/coffeeImages/caramelDark.jpeg",
     "Caramel Dark",
     199,
     false
   ),
   new Coffee(
-    "../assets/coffeeImages/caramelLight.jpeg",
+    "/src/assets/coffeeImages/caramelLight.jpeg",
     "Caramel Light",
     199,
     true
   ),
-  new Coffee("../assets/coffeeImages/chocolate.jpeg", "Chocolate", 149, true),
-  new Coffee("../assets/coffeeImages/vanilla.jpeg", "Vanila", 149, true),
-  new Coffee("../assets/coffeeImages/cinnamon.jpeg", "Cinnamon", 129, false),
-  new Coffee("../assets/coffeeImages/darkRoast.jpeg", "Dark Roast", 199, true),
+  new Coffee("/src/assets/coffeeImages/chocolate.jpeg", "Chocolate", 149, true),
+  new Coffee("/src/assets/coffeeImages/vanilla.jpeg", "Vanila", 149, true),
+  new Coffee("/src/assets/coffeeImages/cinnamon.jpeg", "Cinnamon", 129, false),
+  new Coffee(
+    "/src/assets/coffeeImages/darkRoast.jpeg",
+    "Dark Roast",
+    199,
+    true
+  ),
 ];
