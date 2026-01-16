9e7ba139d295eff9f54ac7e26533f9574d76afe9
Crol91 <karl.e.andersson@hotmail.com>
2026-01-16 11:55:26 +0100

    added a description to coffee objects

--- src/data/coffees.ts
+++ src/data/coffees.ts
@@ -2,24 +2,45 @@ import { Coffee } from "../models/Coffee";
 
 export const coffeeList: Coffee[] = [
   new Coffee(
-    "/src/assets/coffeeImages/caramelDark.jpeg",
-    "Caramel Dark",
+    "/src/assets/coffeeImages/caramelLight.jpeg",
+    "Caramel Light",
+    "Ljustrostat kaffe med kolasmak",
     199,
+    true
+  ),
+  new Coffee(
+    "/src/assets/coffeeImages/chocolate.jpeg",
+    "Chocolate",
+    "Kraftfullt kaffe med med chokladsmak",
+    149,
+    true
+  ),
+  new Coffee(
+    "/src/assets/coffeeImages/vanilla.jpeg",
+    "Vanila",
+    "Ljusrostat kaffe med len vaniljton",
+    149,
+    true
+  ),
+  new Coffee(
+    "/src/assets/coffeeImages/cinnamon.jpeg",
+    "Cinnamon",
+    "Kryddigt kaffe med toner av kanel",
+    129,
     false
   ),
   new Coffee(
-    "/src/assets/coffeeImages/caramelLight.jpeg",
-    "Caramel Light",
+    "/src/assets/coffeeImages/darkRoast.jpeg",
+    "Dark Roast",
+    "Mörkrostat kaffe med intensiv smak",
     199,
     true
   ),
-  new Coffee("/src/assets/coffeeImages/chocolate.jpeg", "Chocolate", 149, true),
-  new Coffee("/src/assets/coffeeImages/vanilla.jpeg", "Vanila", 149, true),
-  new Coffee("/src/assets/coffeeImages/cinnamon.jpeg", "Cinnamon", 129, false),
   new Coffee(
-    "/src/assets/coffeeImages/darkRoast.jpeg",
-    "Dark Roast",
+    "/src/assets/coffeeImages/caramelDark.jpeg",
+    "Caramel Dark",
+    "Mörkrostat kaffe med kolasmak",
     199,
-    true
+    false
   ),
 ];
